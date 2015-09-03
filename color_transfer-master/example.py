3a1ced81fc84c0b1c6d665567822e2bd6a40e99d
monish_godhia <monish1247@gmail.com>
2015-09-03 23:58:43 +0530

    Super fast Color transfer between images using openCV

---
+++ color_transfer-master/example.py
@@ -0,0 +1,36 @@
+
+from color_transfer import color_transfer
+import numpy as np
+import argparse
+import cv2
+
+def show_image(title, image, width = 300):
+	
+	r = width / float(image.shape[1])
+	dim = (width, int(image.shape[0] * r))
+	resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
+	
+	cv2.imshow(title, resized)
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--source", required = True,
+	help = "Path to the source image")
+ap.add_argument("-t", "--target", required = True,
+	help = "Path to the target image")
+ap.add_argument("-o", "--output", help = "Path to the output image (optional)")
+args = vars(ap.parse_args())
+
+
+source = cv2.imread(args["source"])
+target = cv2.imread(args["target"])
+
+transfer = color_transfer(source, target)
+
+
+if args["output"] is not None:
+	cv2.imwrite(args["output"], transfer)
+
+show_image("Source", source)
+show_image("Target", target)
+show_image("Transfer", transfer)
+cv2.waitKey(0)
